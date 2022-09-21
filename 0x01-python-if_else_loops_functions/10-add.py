7adb314ad10dbccae85a7323b73448bbe1164fd5
Samuel Akwensivie <sakwensivie@gmail.com>
2022-09-21 14:16:33 +0000

    finishing up

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def add(a, b):
+    '''Adds the two numbers a and b and returns the sum'''
+    sum = a + b
+    return (sum)
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+def print_last_digit(num):
+    '''prints and returns the last digit of the input num'''
+    if num >= 0:
+        last_digit = num % 10
+    else:
+        last_digit = num % -10
+        last_digit *= -1
+
+    print('{:d}'.format(last_digit), end='')
+    return (last_digit)
