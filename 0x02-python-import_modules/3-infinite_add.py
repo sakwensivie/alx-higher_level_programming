a81f7ac89227f6bf0e7a92880de4d6bc99eddf31
Samuel Akwensivie <sakwensivie@gmail.com>
2022-09-25 17:08:47 +0000

    first commit for today

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import sys as s
+    i = len(s.argv)
+    sum = 0
+    if i > 1 :
+        for arg in s.argv:
+            if arg != s.argv[0]:
+                sum += int(arg)
+
+    print('{}'.format(sum))
