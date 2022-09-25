c4a06c4898f943dd77b967ee05803e51a703f5c9
Samuel Akwensivie <sakwensivie@gmail.com>
2022-09-25 17:31:40 +0000

    corrections

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,7 +3,7 @@ if __name__ == '__main__':
     import sys as s
     i = len(s.argv)
     sum = 0
-    if i > 1 :
+    if i > 1:
         for arg in s.argv:
             if arg != s.argv[0]:
                 sum += int(arg)
