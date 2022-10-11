f1d7189c69af7ff6451244b5840626a98a7af303
Samuel Akwensivie <sakwensivie@gmail.com>
2022-10-11 08:13:39 +0000

    safe print correction

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -12,7 +12,7 @@ def list_division(my_list_1, my_list_2, list_length):
             result = 0
         except IndexError:
             print('out of range')
-            result = 0
+            break
         finally:
             new_list.append(result)
 
