8d15993cc0fb5385bf8af8c960a8f4a0eb9026e9
Samuel Akwensivie <sakwensivie@gmail.com>
2022-10-12 08:44:30 +0000

    polishing the documentation

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+'''A module that defines a class for square
+'''
+
+
+class Square:
+    '''Class that defines a square
+    '''
+    def __init__(self, size=0):
+        '''Initializes the class with certain paramemters
+            Args:
+                size (int): size of the square
+        '''
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+    def area(self):
+        '''Returns the area of the square
+        '''
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        """ Method to returns the size value
+
+            Returns: size of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Method to set the size value of the square object
+
+            args:
+                value (int): size of the square to be set
+
+            Returns: None
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = int(value)
