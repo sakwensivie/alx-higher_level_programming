4ad1323ff5a03db2aa056eb633b99d33e4c61feb
Samuel Akwensivie <sakwensivie@gmail.com>
2022-10-17 21:38:13 +0000

    corrections

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,7 +9,7 @@ class Rectangle:
     The rectangle class
     '''
 
-    def __init__(self, width = 0, height = 0):
+    def __init__(self, width=0, height=0):
         '''
         The init method
 
