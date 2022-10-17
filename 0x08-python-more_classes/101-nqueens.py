7834b32c59638deae5b02046ed82dede5504b163
Samuel Akwensivie <sakwensivie@gmail.com>
2022-10-17 22:59:04 +0000

    nqueens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -63,7 +63,7 @@ def Queen(m_queen, nqueen):
 
     m_queen[nqueen] = -1
 
-    while((m_queen[nqueen] < len(m_queen) - 1)):
+    while ((m_queen[nqueen] < len(m_queen) - 1)):
 
         m_queen[nqueen] += 1
 
@@ -96,7 +96,7 @@ if __name__ == '__main__':
 
     try:
         size = int(sys.argv[1])
-    except:
+    except (ValueError, TypeError):
         print("N must be a number")
         sys.exit(1)
 
