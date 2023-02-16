9dff1096d7e20117e1ab7df505774feab39bfdcd
Samuel Akwensivie <sakwnesivie@gmail.com>
2023-02-16 19:18:13 +0000

    making it executable

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
-curl -s "$1" -d "@$2" -X POST -H "Content-Type: application/json"
+# Script that makes a request to causes an specific response
+curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -H "Origin:HolbertonSchool"
