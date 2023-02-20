1b3f0d4772647c27d39eb3031a61c58281699cb0
Samuel Akwensivie <sakwnesivie@gmail.com>
2023-02-20 14:50:48 +0000

    correction

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
-exports.esrever= function (list) {
+
+exports.esrever = function (list) {
   let len = list.length - 1;
   let i = 0;
   while ((len - i) > 0) {
     const aux = list[len];
     list[len] = list[i];
     list[i] = aux;
-    i++
+    i++;
     len--;
   }
   return list;
