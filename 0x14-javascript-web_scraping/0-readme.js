9b9bed3c91bfd452d4a0d2fc1f69cee3daac5ed3
Samuel Akwensivie <sakwnesivie@gmail.com>
2023-02-20 11:33:06 +0000

    corrections

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', function (err, contents){
-	if (contents === undefined){
-		console.log(err);
+  if (contents === undefined){
+    console.log(err);
   } else {
-		console.log(contents);
-	}
+    console.log(contents);
+  }
 });
